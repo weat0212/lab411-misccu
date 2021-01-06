001027ec550389ec33fadd532b10ccd61df3f670
Andy Wang <weat0212@gmail.com>
2021-01-07 01:10:46 +0800

    Init project

---
+++ src/main/java/ccu/mis/lab411/Lab411Application.java
@@ -0,0 +1,13 @@
+package ccu.mis.lab411;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab411Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab411Application.class, args);
+    }
+
+}
